113289cf3e4df451d20a471996718c930908fafe
Vishal Gupta <yesitsvishal@tatanagar.com>
2021-11-06 03:23:48 +0530

    Add http client code

--- src/main/java/com/youtube/search/client/HttpClient.java
+++ src/main/java/com/youtube/search/client/HttpClient.java
@@ -1,4 +1,86 @@
 package com.youtube.search.client;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 public class HttpClient {
+
+    private CloseableHttpClient client;
+
+    public HttpClient(CloseableHttpClient client) {
+
+        this.client = client;
+    }
+
+    private String execute(String url, Map<String, String> headers, HttpRequestBase httpRequest) throws HttpException, IOException {
+
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            httpRequest.addHeader(entry.getKey(), entry.getValue());
+        }
+
+        CloseableHttpResponse response = null;
+        try {
+            response = client.execute(httpRequest);
+            String contentJson = EntityUtils.toString(response.getEntity());
+            if(response == null || response.getStatusLine() == null){
+                throw new HttpException("Something went wrong");
+            }
+            return contentJson;
+        } catch (Exception e) {
+            log.error("Failed to execute {} on {} {}", httpRequest.getMethod(), url, e.getMessage());
+            throw new HttpException(e.getMessage() + "status code:" + response.getStatusLine().getStatusCode() ,e);
+        } finally {
+            httpRequest.releaseConnection();
+            if (response != null)
+                response.close();
+        }
+    }
+
+    public String post(String url, String json) throws Exception {
+        return post(url, json, Collections.emptyMap());
+    }
+
+    public String post(String url, String json, Map<String, String> headers) throws URISyntaxException, IOException, HttpException {
+        URI uri = new URI(url);
+        HttpPost httpPost = new HttpPost(uri);
+        httpPost.setEntity(new StringEntity(json, StandardCharsets.UTF_8));
+        return execute(url, headers, httpPost);
+    }
+
+    public String get(String url, Map<String, String> queryParams, Map<String, String> headers) throws URISyntaxException, IOException, HttpException {
+        URIBuilder uriBuilder = new URIBuilder(url);
+
+        for (Map.Entry<String, String> queryParam : queryParams.entrySet()) {
+            uriBuilder.addParameter(queryParam.getKey(), queryParam.getValue());
+        }
+
+        URI uri = uriBuilder.build();
+        HttpGet httpGet = new HttpGet(uri);
+        return execute(url, headers, httpGet);
+    }
+
+    public static Map<String, String> headers() {
+
+        Map<String, String> header = new HashMap<>();
+        header.put("Accept", "application/json");
+        header.put("Content-Type", "application/json; charset=UTF-8");
+        return header;
+    }
 }
--- src/main/java/com/youtube/search/client/YoutubeClient.java
+++ src/main/java/com/youtube/search/client/YoutubeClient.java
@@ -1,4 +1,68 @@
 package com.youtube.search.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.youtube.exception.YoutubeResponseParseException;
+import com.youtube.exception.YoutubeSearchException;
+import com.youtube.search.config.SearchAppConfig;
+import com.youtube.search.model.SearchRequest;
+import com.youtube.search.model.response.youtube.SearchResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpException;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+@Slf4j
 public class YoutubeClient {
+
+    private HttpClient httpClient;
+    private SearchAppConfig config;
+    private ObjectMapper objectMapper;
+
+    @Inject
+    public YoutubeClient(@Named("youtubeHttpClient") CloseableHttpClient httpClient, SearchAppConfig config,
+                         ObjectMapper objectMapper) {
+        this.httpClient = new HttpClient(httpClient);
+        this.config = config;
+        this.objectMapper = objectMapper;
+    }
+
+    public SearchResponse search(SearchRequest searchRequest) throws YoutubeSearchException, YoutubeResponseParseException {
+
+        Map<String, String> queryParam = Maps.newHashMap();
+        queryParam.put("key", config.getGoogleSearchApiKey());
+        queryParam.put("q", searchRequest.getQuery());
+        queryParam.put("type", searchRequest.getType());
+        queryParam.put("part", StringUtils.join(searchRequest.getPart(), ","));
+        queryParam.put("publishedAfter", searchRequest.getPublishedAfter());
+
+        String response = null;
+        log.info("Youtube Query: {}", searchRequest.getQuery());
+        try {
+            response = httpClient.get(config.getGoogleSearchApi(), queryParam, HttpClient.headers());
+        } catch (URISyntaxException e) {
+            log.error("Error while creating youtube url, {}, {}", e.getMessage(), e.getReason());
+            throw new YoutubeSearchException("Youtube URI ERROR", e);
+        } catch (IOException e) {
+            log.error("Error while closing youtube http connection, {}, {}", e.getMessage(), e.getCause());
+            throw new YoutubeSearchException("Youtube Http Connection Error", e);
+        } catch (HttpException e) {
+            log.error("Youtube http request failure, {}, {}", e.getMessage(), e.getCause());
+            throw new YoutubeSearchException(e.getMessage(), e);
+        }
+
+        try {
+            SearchResponse searchResponse = objectMapper.readValue(response, SearchResponse.class);
+            return searchResponse;
+        } catch (IOException e) {
+            log.error("Error while parsing youtubr response, {}, {}", e.getMessage(), e.getCause());
+            throw new YoutubeResponseParseException("response:" + response, e);
+        }
+    }
 }
